ab63d027e81b5ff10933f46d0da2f5049649b3b3
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-10-16 22:22:58 +0530

    file added

--- components/BookByTitle.jsx
+++ components/BookByTitle.jsx
@@ -2,6 +2,18 @@ import useFetch from "../useFetch";
 
 const BookByTitle = (title) => {
     const { data, loading, error } = useFetch(`https://book-chi-indol.vercel.app/api/${title}`) 
+
+    return data ? (
+        <div>  
+            <h2>Title: {data.title}</h2>
+            <p>Author: {data.author}</p>
+            <p>PublishedYear: {data.publishedYear}</p>
+            <p>Language: {data.language}</p>
+            <p>Country: {data.country}</p>
+        </div>
+    ) : (
+        loading && <p>Loading...</p>
+    )
 }
 
 export default BookByTitle;
