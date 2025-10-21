53b6d1f5f8179b4122780624fc50621c31d224ff
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-10-21 14:13:47 +0530

    form data

--- components/Book.jsx
+++ components/Book.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import useFetch from "../useFetch";
 
 const Book = () => {
-
+    const [successMessage, setSuccessMessage] = useState("");
     const { data, loading, error } = useFetch("https://book-chi-indol.vercel.app/books");
     console.log(data);
 
